8e43720ac6622f556eb8f29a6a6b0c236b53b7ca
ltheo <theo.luthin@ynov.com>
2026-09-11 14:17:01 +0200

    fix venv problem

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import auth 
+
+app = FastAPI(title="Smart Fridge et Nutrition Coach")
+app.include_router(auth.router)
+
+# CORS (tu pourras ajuster plus tard)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "Bienvenue dans le Smart Fridge & Nutrition Coach !"}
