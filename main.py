7aa959782f6b4770b9f82e8aeba241a856091f73
ltheo <theo.luthin@ynov.com>
2026-09-11 14:51:27 +0200

    fix error on main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import auth 
+from routers import auth 
 
 app = FastAPI(title="Smart Fridge et Nutrition Coach")
 app.include_router(auth.router)
